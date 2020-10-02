b578a15f031b7621b1c999018cfd689dfe805a40
Harshit Raj <harshitraj712@gmail.com>
2020-10-03 00:12:24 +0530

    searc function added

--- main.js
+++ main.js
@@ -54,6 +54,7 @@ function deleteItem(){
   buttonArray = document.querySelectorAll(".delButton")
   buttonArray.forEach(el => {
     el.addEventListener("click",() => {
+      search.value = ""
       parent = el.parentElement
       let index = parent.id
       elArray.splice(index,1)
@@ -61,21 +62,46 @@ function deleteItem(){
       idVar = 0
       listArea.innerHTML = ""
       elArray.forEach(addLi)
+      alert("item is going to be deleted!")
     })
   })
 }
 
-search.addEventListener('keyup', function (event) {
-      if (event.keyCode === 13) {
-         if(search.value.length > 0){
-           searcher(search.value)
-         }
-        else{
-          alert("The input field is empty!")
-        }
-      }
-   });
+search.addEventListener('keyup',() => {
+  if(search.value.length > 0){
+             searcher(search.value)
+           }
+  else{
+    listArea.innerHTML = ""
+    idVar = 0
+    elArray.forEach(addLi)
+  }
+
+} );
 
 function searcher(value){
   listArea.innerHTML = ""
+  for(let i = 0; i< elArray.length; i++){
+    let str = elArray[i]
+    let regex = new RegExp(value,"i");
+    if(str.match(regex) !== null){
+      let el = `<li class="item-type" id=${i}>${str}
+     <button type="button" name="button" class="delButton"><i class="fa fa-times fa-2x" aria-hidden="false"></i></button>
+      </li>`
+      listArea.innerHTML += el
+      hoverbutton()
+      deleteItem()
+    }
+  }
 }
+
+// function (event) {
+//       if (event.keyCode === 13) {
+//          if(search.value.length > 0){
+//            searcher(search.value)
+//          }
+//         else{
+//           alert("The input field is empty!")
+//         }
+//       }
+//    }
